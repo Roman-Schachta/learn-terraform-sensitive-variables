fd87299cb13fac4b45ea2d2299335f02585a7a90
Roman-Schachta <71693811+Roman-Schachta@users.noreply.github.com>
2021-05-02 19:50:59 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,6 +4,18 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
+variable "db_username" {
+  description = "Database administrator username"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  type        = string
+  sensitive   = true
+}
+
 variable "vpc_cidr_block" {
   description = "CIDR block for VPC"
   type        = string
